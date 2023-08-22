245f33af59eb0e9228d57f842958781d6a3adc3a
gaviprasad <gavi@gmail.com>
2023-08-22 07:38:26 +0530

    Added Cultimax fetch pro endpoint

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express")
 const app = express()
 const axios = require("axios")
+app.get("/",(req,res)=>{
+    res.send("Cultimax FetchPro")
+})
 const callApi = ()=>{
     axios.get("https://cultimaxcd.onrender.com").then((resp)=>console.log(resp.data)).catch((e)=>console.log(e));
     axios.get("https://cultimaxd2.onrender.com").then((resp)=>console.log(resp.data)).catch((e)=>console.log(e));    
     axios.get("https://cultimax.onrender.com").then((resp)=>console.log("cultimaxmain")).catch((e)=>console.log(e));    
+    axios.get("https://fetchpro.onrender.com").then((resp)=>console.log(resp.data)).catch((e)=>console.log(e));    
 }
 setInterval(()=>{
     callApi()
 },600000)
 
 app.listen(5000,()=>{
+    callApi()
     console.log('server is running on port 5000')
 })
